9f32685417f98d01286a6e5edcd43b242b819132
Nikhil Dhiman <dhimannikhil905@gmail.com>
2023-05-15 14:08:05 +0530

    Made the FC-Website responsive

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const servicebtn = document.querySelector('.service');
 const dropdown = document.querySelector('.dropdown');
 const links = document.querySelectorAll('.animated-link');
+const  hamb = document.querySelector('.ham-menu');
+const sidenav = document.querySelector('.headerlinks')
 
 servicebtn.addEventListener('mouseenter', () => {
     dropdown.classList.add('active');
-})
+});
 dropdown.addEventListener('mouseleave', () => {
     dropdown.classList.remove('active');
+});
+hamb.addEventListener('click',() => { 
+   sidenav.classList.toggle('active')
+
 })
