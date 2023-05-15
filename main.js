797bb733be4879e6e62452c29bded11a41a5c351
Nikhil Dhiman <dhimannikhil905@gmail.com>
2023-05-15 18:15:11 +0530

    last update of 15th may

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ const servicebtn = document.querySelector('.service');
 const dropdown = document.querySelector('.dropdown');
 const links = document.querySelectorAll('.animated-link');
 const  hamb = document.querySelector('.ham-menu');
-const sidenav = document.querySelector('.headerlinks')
+const sidenav = document.querySelector('.headerlinks');
+const footeraccordions = document.querySelectorAll(".footerCol2");
+const footerlinks = document.querySelectorAll(".acc-links");
+
 
 servicebtn.addEventListener('mouseenter', () => {
     dropdown.classList.add('active');
@@ -12,5 +15,9 @@ dropdown.addEventListener('mouseleave', () => {
 });
 hamb.addEventListener('click',() => { 
    sidenav.classList.toggle('active')
-
+});
+footeraccordions.forEach((footeraccordion) => {
+    footeraccordion.addEventListener('click',()=> {
+    footeraccordion.classList.toggle('active')
+    })
 })
